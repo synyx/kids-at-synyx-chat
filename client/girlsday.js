119bbdaea0b4ec0f4fa41137b1a095e7aaba6fa3
Julia Burgard <burgard@synyx.de>
2018-03-23 10:53:24 +0100

    Add initial client code

---
+++ client/girlsday.js
@@ -0,0 +1,14 @@
+$(function () {
+    var socket = io("http://localhost:3000");
+
+    $('form').submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+});
