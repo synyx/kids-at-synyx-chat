8fc67927e44ef91e18f43a63bf62545ac720c303
Ben Antony <antony@synyx.de>
2018-03-23 11:32:34 +0100

    Add logging to server

--- server/index.js
+++ server/index.js
@@ -3,15 +3,29 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/dashboard.html', function(req, res){
+  res.sendFile(__dirname + '/dashboard.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    let user = msg.user || undefined;
+    let message = msg.message;
+    let timestamp = getFormattedDate();
+    console.log(timestamp + ' user: ' + user + ' send message: ' + message);
+    var result = {
+      user: user,
+      message: message,
+      timestamp: timestamp
+    };
+    io.emit('chat message', result);
   });
 });
+
+function getFormattedDate() {
+    var date = new Date();
+    return date.toString();
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
