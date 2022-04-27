47269af22afc7d52a2c7702e2c6652992234a213
Patrick helm <helm@synyx.de>
2022-04-27 15:29:36 +0200

    renaming action

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 8080;
 
-app.use(express.static('app'));
+app.use(express.static('public'));
 
 http.listen(port, function(){
     console.log('listening on *:' + port);
